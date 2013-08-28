b1c8d382397390a7c4cc0666dddc4306ce5d1f55
tiberiuc <tibi@happysoft.ro>
2013-08-28 12:35:10 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp zshrc      ~/.zshrc
+cp dir-colors ~/.dir-colors
+
+
+CONFIG_MC=~/.config/mc
+
+mkdir -p ${CONFIG_MC}
+
+cp ./mc/ini        ${CONFIG_MC}/ 
+cp ./mc/panels.ini ${CONFIG_MC}/
+
+MC_SKIN_FOLDER=/usr/share/mc/skins
+sudo cp ./mc/solarized.ini ${MC_SKIN_FOLDER}/
