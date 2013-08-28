86a7d4f15e9b2268c75888673783f24333e31ea7
tiberiuc <tibi@happysoft.ro>
2013-08-28 12:36:49 +0300

    Create install-configs.sh

---
+++ install-configs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp zshrc      ~/.zshrc
+cp dir-colors ~/.dir-colors
+
+
+CONFIG_MC=~/.config/mc
+
+mkdir -p ${CONFIG_MC}
+
+cp ./mc/ini        ${CONFIG_MC}/ 
+cp ./mc/panels.ini ${CONFIG_MC}/
+
+MC_SKIN_FOLDER=/usr/share/mc/skins
+sudo cp ./mc/solarized.ini ${MC_SKIN_FOLDER}/
+
+git config --global alias.co checkout
