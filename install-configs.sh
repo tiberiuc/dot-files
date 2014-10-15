7d3fd9c5434c4f94e1c8613e05ff88aee60ca993
Tiberiu Craciun <tibi@happysoft.ro>
2014-10-15 12:22:38 +0300

    Added awesome and terminator to install

--- install-configs.sh
+++ install-configs.sh
@@ -7,11 +7,18 @@ cp  ctags  ~/.ctags
 
 
 CONFIG_MC=~/.config/mc
+CONFIG_AWESOME=~/.config/awesome
+CONFIG_TERMINATOR=~/.config/terminator
 
 mkdir -p ${CONFIG_MC}
+mkdir -p ${CONFIG_AWESOME}
+mkdir -p ${CONFIG_TERMINATOR}
 
 cp ./mc/ini        ${CONFIG_MC}/
 cp ./mc/panels.ini ${CONFIG_MC}/
+
+cp -r ./awesome/* ${CONFIG_AWESOME}/
+cp -r ./terminator/* ${CONFIG_TERMINATOR}/
 
 MC_SKIN_FOLDER=/usr/share/mc/skins
 sudo cp ./mc/solarized.ini ${MC_SKIN_FOLDER}/
