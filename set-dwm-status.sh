27d8af0310bb56339d728e29fd7469862cba1486
Tiberiu Craciun <tibi@happysoft.ro>
2014-11-29 19:28:37 +0200

    Added solarized powerline like status

--- set-dwm-status.sh
+++ set-dwm-status.sh
@@ -1,4 +1,79 @@
 #!/bin/sh
 
-export DISPLAY=:0; xsetroot -name "Mem $(free -m | grep '^Mem' | awk '{$2/=1024;$3/=1024;printf "%.2f GB / %.2f GB",$3,$2 }') | CPU $(top -bn 1 | grep 'Cpu' | tr -d 'usy,' | awk '{print "user " $2 ", sys " $3}') | Bat $(upower -i `upower -e | grep BAT` | grep --color=never -E "percentage" | cut -d " " -f 15) | $( date +"%a %b %R")" > /dev/null 2>&1
+# icons at  https://www.dropbox.com/s/9iysh2i0gadi4ic/icons.pdf
+
+
+base03="#002b36"
+base02="#073642"
+base01="#586e75"
+base00="#657b83"
+base0="#839496"
+base1="#93a1a1"
+base2="#eee8d5"
+base3="#fdf6e3"
+yellow="#b58900"
+orange="#cb4b16"
+red="#dc322f"
+magenta="#d33682"
+violet="#6c71c4"
+blue="#268bd2"
+cyan="#2aa198"
+green="#859900"
+
+
+memory () {
+    echo -e '\uf16c ' $(free -m | grep '^Mem' | awk '{$2/=1024;$3/=1024;printf "%.2f / %.2f GB",$3,$2 }')
+}
+
+cpu () {
+    echo -e '\uf0e4 ' $(top -bn 1 | grep 'Cpu' | tr -d 'usy,' | awk '{print "user " $2 ", sys " $3}')
+}
+
+power () {
+    echo -e '\uf215 ' $(upower -i `upower -e | grep BAT` | grep --color=never -E "percentage" | cut -d " " -f 15)
+}
+
+
+show_date () {
+    echo $(date +"%a %b %d %R")
+}
+
+arrow () {
+    prevColor=$1
+    color=$2
+    echo "<span color=\"$color\" bgcolor=\"$prevColor\"></span>"
+}
+
+
+section () {
+    prevColor=$1
+    color=$2
+    text=$3
+    echo "$(arrow $prevColor $color)<span color=\"white\" bgcolor=\"$color\"> $text </span>"
+}
+
+section_memory () {
+    echo "$(section 'black' $base01 "$(memory)")"
+}
+
+
+section_cpu () {
+    echo "$(section $base01 $base02 "$(cpu)")"
+}
+
+section_power () {
+    echo "$(section $base02 $base1 "$(power)")"
+}
+
+section_date () {
+    echo "$(section $base1 $base00 "$(show_date)")"
+}
+
+status="$(memory) | $(cpu) | $(power) | $(show_date)"
+status="$(section_memory)$(section_cpu)$(section_power)$(section_date)"
+
+echo $status
+
+export DISPLAY=:0; xsetroot -name "$status" > /dev/null 2>&1
+
 
